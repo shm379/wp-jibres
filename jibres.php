c5b917e59756138b26063f96155e4e7992ecb213
sarbazk <me@sarbazk.com>
2020-02-26 17:02:42 +0330

    update jibres

--- jibres.php
+++ jibres.php
@@ -66,12 +66,12 @@ function admin_jib()
 		}
 		else
 		{
-			printf('<a href="?page=jibres&jibres=backup_all"><button class="bt">Backup All Data</button></a>');
+			printf('<a href="?page=jibres&jibres=backup_all"><button class="bt">Backup All Data</button></a><br><br>');
 			printf('<a href="?page=jibres&jibres=products_backup"><button class="bt">Backup Your Products</button></a><br><br>');
 			printf('<a href="?page=jibres&jibres=orders_backup"><button class="bt">Backup Your Orders</button></a><br><br>');
 			printf('<a href="?page=jibres&jibres=posts_backup"><button class="bt">Backup Your Posts</button></a><br><br>');
 			printf('<a href="?page=jibres&jibres=comments_backup"><button class="bt">Backup Your Comments</button></a><br><br>');
-			printf('<a href="?page=jibres&jibres=categories_backup"><button class="bt">Backup Your Categories</button></a><br><br>');
+			printf('<a href="?page=jibres&jibres=categories_backup"><button class="bt">Backup Your Categories</button></a>');
 		}
 		printf('</div>');
 		
