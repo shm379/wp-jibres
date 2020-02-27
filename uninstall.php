4eac0d1161f4545fad46b556d83227ef70b9f687
sarbazk <me@sarbazk.com>
2020-02-27 14:20:10 +0330

    change uninstall

--- uninstall.php
+++ uninstall.php
@@ -5,12 +5,14 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
  
-$option_name = 'jibres';
+
+// the options in {$wpdb->prefix}options
+// $option_name = 'jibres';
  
-delete_option($option_name);
+// delete_option($option_name);
  
-// for site options in Multisite
-delete_site_option($option_name);
+// // for site options in Multisite
+// delete_site_option($option_name);
  
 // drop a custom database table
 global $wpdb;
