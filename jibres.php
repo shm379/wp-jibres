d15cc3517aa849ad6dda7d80ebdaed5a8e8d59e7
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-28 23:18:34 +0330

    check double instance

--- jibres.php
+++ jibres.php
@@ -2,24 +2,48 @@
 /**
  * @package           Jibres
  * @copyright         2020 Jibres
+ * @link              https://Jibres.com
  *
  * Plugin Name:       Jibres
  * Plugin URI:        https://github.com/jibres/wp-jibres
  * Description:       Backup of all of your wordpress data and woocommerce into Jibres. Anytime you want you can transfer to Jibres. #1 World Sales Engineering System. Sell & Enjoy
- * Version: 		  		1.1
+ * Version:           1.1
  * Author:            Jibres
  * Author URI:        https://Jibres.com
+ * Text Domain:       jibres
+ * Domain Path:       /languages
 */
 
+// If this file is called directly, abort.
+if (!defined('WPINC'))
+{
+	die('Hey There! No Naughty Business Please!');
+}
+
+// Abort loading if WordPress is upgrading
+if (defined('WP_INSTALLING') && WP_INSTALLING)
+{
+	return;
+}
+
+function jibres_two_instance_notice()
+{
+	echo '<div class="error"><p>' . __( 'You are using two instances of Jibres plugin at same time! Please deactive one of them.', 'jibres' ) . '</p></div>';
+}
+// check whether another instance of Master Slider is activated or not
+if(defined('JIBRES_VERSION'))
+{
+	add_action( 'admin_notices', 'jibres_two_instance_notice' );
+	return;
+}
+
 // Make sure we don't expose any info if called directly
-if ( !function_exists( 'add_action' ) ) {
+if ( !function_exists( 'add_action' ) )
+{
 	echo "Hi there! I'm just a plugin, not much I can do when called directly.";
 	exit;
 }
-if(defined('JIBRES_DIR'))
-{
-	echo "wooow! What's happening!";
-}
+
 
 define('JIBRES_DIR', dirname(__FILE__). DIRECTORY_SEPARATOR);
 define('JIBRES_VERSION', '1.1');
