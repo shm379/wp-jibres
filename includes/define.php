e21805767e9c7d4bd5db61569c6896582b03e201
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-02-29 01:06:42 +0330

    enable debug

--- includes/define.php
+++ includes/define.php
@@ -1,5 +1,6 @@
 <?php
 
+define('WP_DEBUG', true);
 define('JIBRES_VERSION', '1.1');
 define('JIBRES_INC', JIBRES_DIR. 'includes/');
 
@@ -9,11 +10,11 @@ define('JIBRES_INC', JIBRES_DIR. 'includes/');
 register_activation_hook(JIBRES_INC. 'activate', 'wp_jibres_activate');
 register_deactivation_hook(JIBRES_INC. 'deactivate', 'wp_jibres_deactivate');
 
-// load translations
 
+// load translations
 $locale = apply_filters( 'plugin_locale', get_locale(), 'jibres' );
 load_textdomain( 'jibres', trailingslashit( WP_LANG_DIR ) . 'jibres' . '/' . 'wp_jibres' . '-' . $locale . '.mo' );
-load_plugin_textdomain('jibres', false, basename(JIBRES_DIR).'/languages/');
+load_plugin_textdomain('jibres', false, basename(JIBRES_DIR).'/languages');
 
 
 ?>
