64847002e5953b04052250aef7b53be9cff8a7f3
sarbazk <me@sarbazk.com>
2020-02-29 06:47:09 +0330

    update first page

--- first_jibres.php
+++ first_jibres.php
@@ -4,7 +4,7 @@ printf('<h1>Jibres</h1>');
 printf('<p>Welcome to the official Jibres plugin for worpress.</p>');
 printf('<p>For backup your data like products, orders, posts, comments and categories into your store in Jibres use this plugin.</p>');
 printf('<p>For backup your data like said examples into csv files again use this plugin.</p>');
-printf('<p>Create your store in Jibres Sell and Enjoy :)</p>');
+printf('<p>Create your store in Jibres .Sell and Enjoy :)</p>');
 printf('<p>More informations in <a href="https://jibres.com" target="_blank" style="font-weight: bold; text-decoration: none;">Jibres</a>.</p>');
 printf('<p>For connect to Jibres api fill out the following information but if you want to backup into csv files without use jibres api only click submit.</p>');
 printf('<p>Csv files path: wp-content/plugins/this plugin folder(wp-jibres)/backup</p>');
