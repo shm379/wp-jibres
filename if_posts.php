132edc04f7219f1e29f61f3906bb886d6dee93b9
sarbazk <me@sarbazk.com>
2020-03-03 13:22:15 +0330

    create class jibres_posts

--- if_posts.php
+++ if_posts.php
@@ -1,51 +1,82 @@
 <?php 
-
-if (isset($_POST['weris'])) 
+/**
+ * @author Jibres
+ */
+class jibres_posts
 {
-	if ($_POST['store'] and !empty($_POST['store']) and !empty($_POST['apikey']) and !empty($_POST['appkey'])) 
-	{
-		$data_posted = array('store' => $_POST['store'], 'apikey' => $_POST['apikey'], 'appkey' => $_POST['appkey'], 'wis' => $_POST['weris']);
-	}
-	else
-	{
-		$data_posted = array('wis' => 'csv');
-	}
-	insert_in_jibres($data_posted, JIBRES_TABLE);
-	header("Refresh:0");
-}
 
-if (isset($_POST['usas'])) 
-{
-		
-	if (isset($_POST['usas']) and $_POST['usas'] == 'api' and ch_jibres_store_data() == false) 
+	private $data = [];
+	
+	function __construct($entry)
 	{
-		ch_jibres_store_data('start_again');
+		$this->data = $entry;
+
+		if (is_array($entry)) 
+		{
+			foreach ($entry as $key => $value) 
+			{
+				if (method_exists($this, $key)) 
+				{
+					$this->$key();
+				}
+			}
+		}	
+	}
+
+
+	private function usas()
+	{
+		global $wpdb;
+		if (isset($this->data['usas']) and $this->data['usas'] == 'api' and ch_jibres_store_data() == false) 
+		{
+			ch_jibres_store_data('start_again');
+			header("Refresh:0");
+		}
+		else
+		{
+			$wpdb->update(
+							$wpdb->prefix . 'jibres',
+							array( 'wis' => $this->data['usas'] ),
+							array( 'id' => 1 )
+						);
+			header("Refresh:0");
+		}
+	}
+
+
+	private function weris()
+	{
+		if (!empty($this->data['store']) and !empty($this->data['apikey']) and !empty($this->data['appkey'])) 
+		{
+			$data_posted = array('store' => $this->data['store'], 'apikey' => $this->data['apikey'], 'appkey' => $this->data['appkey'], 'wis' => $this->data['weris']);
+		}
+		else
+		{
+			$data_posted = array('wis' => 'csv');
+		}
+		insert_in_jibres($data_posted, JIBRES_TABLE);
 		header("Refresh:0");
 	}
-	else
+
+
+	private function changit()
 	{
-		$wpdb->update(
-						$wpdb->prefix . 'jibres',
-						array( 'wis' => $_POST['usas'] ),
-						array( 'id' => 1 )
-					);
+		ch_jibres_store_data($this->data['changit']);
 		header("Refresh:0");
+	}
+
+
+	private function csvdel()
+	{
+		global $wpdb;
+		$del_data = explode("_", $this->data['csvdel']);
+		$wpdb->delete( JIBRES_CTABLE, array( 'type' => $del_data[1], 'wers' => 'csv' ) );
+		unlink(JIBRES_DIR. 'backup/'. $del_data[0]. '.csv');
 	}
 }
 
 
-if (isset($_POST['changit'])) 
-{
-	ch_jibres_store_data($_POST['changit']);
-	header("Refresh:0");
-}
+$jibres_answer_post = new jibres_posts($_POST);
 
-
-if (isset($_POST['csvdel'])) 
-{
-	$del_data = explode("_", $_POST['csvdel']);
-	$wpdb->delete( JIBRES_CTABLE, array( 'type' => $del_data[1], 'wers' => 'csv' ) );
-	unlink(JIBRES_DIR. 'backup/'. $del_data[0]. '.csv');
-}
 
 ?>
