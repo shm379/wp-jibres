6f8eafbdbf798e0119b26cdb663716f5f41b6552
sarbazk <me@sarbazk.com>
2020-03-15 13:14:09 +0330

    update jibres main class

--- includes/class-wp-jibres.php
+++ includes/class-wp-jibres.php
@@ -12,18 +12,18 @@ class jibres_start
 	function __construct()
 	{
 		$table = self::$jibres_t;
-		 $strc =  "CREATE TABLE $table (
-		  				id int(11) NOT NULL AUTO_INCREMENT,
-		  				time datetime DEFAULT NOW() NOT NULL,
-		  				store varchar(11) DEFAULT NULL,
-		  				token varchar(255) DEFAULT NULL,
-		  				appkey varchar(32) DEFAULT NULL,
-		  				apikey varchar(32) DEFAULT NULL,
-		  				phone_number varchar(20) DEFAULT NULL,
-		  				login tinyint(1) DEFAULT '0',
-		  				wis varchar(55) NOT NULL,
-		  				PRIMARY KEY  (id)
-						) $charset_collate;";
+		$strc = "CREATE TABLE $table (
+		  		 id int(11) NOT NULL AUTO_INCREMENT,
+		  		 time datetime DEFAULT NOW() NOT NULL,
+		  		 store varchar(11) DEFAULT NULL,
+		  		 token varchar(255) DEFAULT NULL,
+		  		 appkey varchar(32) DEFAULT NULL,
+		  		 apikey varchar(32) DEFAULT NULL,
+		  		 phone_number varchar(20) DEFAULT NULL,
+		  		 login tinyint(1) DEFAULT '0',
+		  		 wis varchar(55) NOT NULL,
+		  		 PRIMARY KEY  (id)
+				 ) $charset_collate;";
 
 
 		if (create_jibres_table($strc, $table) === true) 
@@ -89,31 +89,47 @@ class jibres_start
 	{
 		global $wpdb;
 
-		require_once JIBRES_INC. 'jibres_backup_class.php';
-			
-		if ( isset($_GET['jibres']) and $_GET['jibres'] == 'backup_all' ) 
+		
+		if ( isset($_GET['jibres']) ) 
 		{
-			$packs = array('products', 'orders', 'posts', 'comments', 'categories');
-			foreach ($packs as $value) 
+			require_once JIBRES_INC. 'jibres_backup_class.php';
+			if ( $_GET['jibres'] == 'backup_all' ) 
 			{
-				require_once JIBRES_INC . $value.'_backup.php';
-				$classname = 'jibres_'.$value;
-				$run_class = new $classname();
+				$this->jibres_backup_all();
 			}
-			printf('<a href="?page=jibres"><button class="bt">Back Home</button></a>');
-		}
-		elseif ( isset($_GET['jibres']) ) 
-		{
-			require_once JIBRES_INC. $_GET['jibres'].'.php';
-			$get_cname = explode("_", $_GET['jibres']);
-			$classname = 'jibres_'. $get_cname[0];
-			$run_class = new $classname();
-			printf('<a href="?page=jibres"><button class="bt">Back Home</button></a>');
+			else
+			{
+				$this->jibres_backup();
+			}
 		}
 		else
 		{
 			require_once JIBRES_INC. 'jibres_main.php';
 		}
+
+	}
+
+
+	private function jibres_backup_all()
+	{
+		$packs = array('products', 'orders', 'posts', 'comments', 'categories');
+		foreach ($packs as $value) 
+		{
+			require_once JIBRES_INC . $value.'_backup.php';
+			$classname = 'jibres_'.$value;
+			$run_class = new $classname();
+		}
+		printf('<a href="?page=jibres"><button class="bt">Back Home</button></a>');
+	}
+
+
+	private function jibres_backup()
+	{
+		require_once JIBRES_INC. $_GET['jibres'].'.php';
+		$get_cname = explode("_", $_GET['jibres']);
+		$classname = 'jibres_'. $get_cname[0];
+		$run_class = new $classname();
+		printf('<a href="?page=jibres"><button class="bt">Back Home</button></a>');
 	}
 
 
